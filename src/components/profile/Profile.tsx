660f05422eba5c26f3e289cc2d3718612cd94306
xw <1162744696@qq.com>
2023-09-20 13:58:24 +0800

    wip: 个人资料个人信息部分

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ProfileWrapper from "./style";
 import Avatar from "../avatar/Avatar";
+import "styled-components/macro"
+import Paragraph from "../paragraph/Paragraph";
 
 import face from "assets/images/OIP.jpg";
+import Emoji from "../emoji/Emoji";
 
 type Props = {
 }
@@ -16,6 +19,31 @@ const Profile: React.FC<Props> = (props) => {
             status={'online'}
             statusIconSize={'25px'}
         />
+        <Paragraph
+            size={"xlarge"}
+            css={`
+              margin-bottom: 12px;
+            `}
+        >
+            李嘉图
+        </Paragraph>
+        <Paragraph
+            size={"medium"}
+            type={'secondary'}
+            css={`
+              margin-bottom: 18px;
+            `}
+        >
+            杭州市
+        </Paragraph>
+        <Paragraph
+            css={`
+              margin-bottom: 26px;
+            `}
+        >
+            帮助客户构建网站，并协助在社交网站上进行推广
+            <Emoji label={'fire'}>😍</Emoji>
+        </Paragraph>
     </ProfileWrapper>
 }
 
