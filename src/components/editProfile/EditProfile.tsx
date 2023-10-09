328866a4edca4d3f6904665793017ba019fc6f87
xw <1162744696@qq.com>
2023-10-09 17:18:39 +0800

    wip: 编辑个人资料组件

--- src/components/editProfile/EditProfile.tsx
+++ src/components/editProfile/EditProfile.tsx
@@ -1,6 +1,19 @@
 import React, {useState} from "react";
-import EditProfileWrapper from "./style";
+import EditProfileWrapper, {GenderAndRegion, GroupTitle, IconInputWrapper, SelectGroup} from "./style";
 import Profile from "../profile/Profile";
+import Avatar from "../avatar/Avatar";
+import Button from "../button/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck} from "@fortawesome/free-solid-svg-icons/faCheck";
+import face from "assets/images/OIP.jpg";
+import "styled-components/macro"
+import InputText from "../input/inputText/InputText";
+import Radio, {RadioGroup} from "../radio/Radio";
+import LabelContainer from "../labelContainer/LabelContainer";
+import Select from "../select/Select";
+import Option from "../option/Option";
+import SocialIcon from "../icon/socialIcon/SocialIcon";
+import {faGithub, faLinkedin, faWeibo} from "@fortawesome/free-brands-svg-icons";
 
 type Props = {
     children?: any
@@ -9,8 +22,69 @@ type Props = {
 const EditProfile: React.FC<Props> = (props) => {
     const [showEdit, setShowEdit] = useState(false)
     return showEdit ? <EditProfileWrapper {...props}>
-        {props.children}
+        <Avatar
+            src={face}
+            size={'160px'}
+            css={`
+              grid-area: 1 / 1 / 2 / 2;
+              justify-self: center;
+              margin-bottom: 12px;
+            `}
+        />
+        <Button
+            size={'52px'}
+            css={`
+              grid-area: 1 / 1 / 3 / 2;
+              z-index: 10;
+              align-self: end;
+              justify-self: end;
+`           }
+            onClick={()=> setShowEdit(false)}
+        >
+            <FontAwesomeIcon icon={faCheck} />
+        </Button>
+        <GroupTitle>基本信息</GroupTitle>
+        <InputText label={'昵称'} />
+        <GenderAndRegion>
+            <RadioGroup label={'性别'}>
+                <Radio name={'gender'}>男</Radio>
+                <Radio name={'gender'}>女</Radio>
+            </RadioGroup>
+            <LabelContainer label={'地区'}>
+                <SelectGroup>
+                    <Select type={'form'}>
+                        <Option >省份</Option>
+                    </Select>
+                    <Select type={'form'}>
+                        <Option >城市</Option>
+                    </Select>
+                    <Select type={'form'}>
+                        <Option >县区</Option>
+                    </Select>
+                </SelectGroup>
+            </LabelContainer>
+        </GenderAndRegion>
+        <GroupTitle>联系信息</GroupTitle>
+        <InputText label={'联系电话'} />
+        <InputText label={'电子邮箱'} />
+        <InputText label={'个人网站'} />
+        <GroupTitle>社交信息</GroupTitle>
+        <IconInput icon={faWeibo} bgColor={'#F06767'} />
+        <IconInput icon={faGithub} bgColor={'black'} />
+        <IconInput icon={faLinkedin} bgColor={'#2483c0'} />
     </EditProfileWrapper> : <Profile onEdit={()=> setShowEdit(true)} showEditButton showCloseIcon={false} />
+}
+
+type IconInputProps = {
+    icon: any
+    bgColor: string
+}
+
+const IconInput: React.FC<IconInputProps> = ({icon,bgColor,...props}) => {
+    return <IconInputWrapper>
+        <SocialIcon icon={icon} bgColor={bgColor} />
+        <InputText {...props} />
+    </IconInputWrapper>
 }
 
 
--- src/components/editProfile/style.ts
+++ src/components/editProfile/style.ts
@@ -1,10 +1,42 @@
 import styled from "styled-components"
+import Text from "../text/Text";
 
 interface StyleProps {
 }
 
+const GroupTitle = styled(Text).attrs({size: 'large'})`
+  align-self: end;
+`
+
 const EditProfileWrapper = styled.div<StyleProps>`
+  display: grid;  
+  grid-template-columns: 1fr;
+  row-gap: 20px;
+  padding: 30px;
+  max-height: 100vh;
+  overflow-y: auto;
+`
+
+const GenderAndRegion = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: space-between;
+`
+
+const SelectGroup = styled.div`
+  > * {
+    margin: 0 4px;
+  }
+  
+  margin: 0 -4px;
+`
+
+const IconInputWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 38px 1fr;
+  align-items: end;
 `
 
 
 export default EditProfileWrapper;
+export {GroupTitle,GenderAndRegion,SelectGroup,IconInputWrapper}
